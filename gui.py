11c6696d848b9ee0fdf26289bdf800adb7456f75
VexHack <dev@vexhack.pl>
2026-05-28 17:29:49 +0200

    fix version to 2.3.1

--- gui.py
+++ gui.py
@@ -14,7 +14,7 @@ from PyQt6.QtCore import Qt, QThread, pyqtSignal, QTimer, QPointF, QRectF
 from PyQt6.QtGui import QFont, QColor, QPainter, QPen, QBrush, QDragEnterEvent, QDropEvent, QFontDatabase, QCursor, QAction, QIcon, QPixmap
 from compression import CMPArchive, VHArchive, CMPCompressor, VHCompressor
 
-VERSION = "2.3.0"
+VERSION = "2.3.1"
 GITHUB_REPO = "wk12100lol-prog/vexhack.vh"
 
 ARCHIVERS = {
